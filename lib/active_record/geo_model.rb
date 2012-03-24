da654e8f8770e452470a2bc8c5b88d8017ab777e
Alexey Noskov <alexey.noskov@gmail.com>
2012-03-24 23:47:09 +0400

    Fixes

--- lib/active_record/geo_model.rb
+++ lib/active_record/geo_model.rb
@@ -2,28 +2,12 @@ module ActiveRecord::GeoModel
 
   extend ActiveSupport::Concern
 
-  def latlng_geometry
-    @latlng_geometry ||= RGeo::Feature.cast(geometry, :factory => self.class.latlng_proj, :project => true)
-  end
-
-  def latlng_centroid
-    @latlng_centroid ||= RGeo::Feature.cast(centroid, :factory => self.class.latlng_proj, :project => true)
-  end
-
   def centroid
     if RGeo::Feature::Point === geometry
       geometry
     else
       geometry.centroid
     end
-  end
-
-  module ClassMethods
-
-    def latlng_proj
-      @latlng_proj ||= RGeo::Geos.factory(:srid => 4326, :proj4 => " +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0", :coord_sys => 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137.0,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0.0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]') or raise StandardError.new("Couldn't initialize geos factory!")
-    end
-
   end
 
 end
