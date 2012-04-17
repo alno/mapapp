9c1b4c1409f130eb015a9bae4ea72bc009d29e7c
Alexey Noskov <alexey.noskov@gmail.com>
2012-04-17 23:42:43 +0400

    More osm links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 
-  def osm_link(model)
+  def osm_browse_link(model)
     link_to t("info.links.osm_browse"), "http://www.openstreetmap.org/browse/#{model.osm_type}/#{model.id}", :target => '_blank'
+  end
+
+  def osm_history_link(model)
+    link_to t("info.links.osm_history"), "http://www.openstreetmap.org/browse/#{model.osm_type}/#{model.id}/history", :target => '_blank'
   end
 
 end
